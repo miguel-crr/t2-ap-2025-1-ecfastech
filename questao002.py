d35e8f74afb5f388d2240a4b901bde8bc5fc8712
miguel-crr <miguelcserraglio@gmail.com>
2025-06-25 15:12:27 -0400

    Add files via upload

---
+++ questao002.py
@@ -0,0 +1,6 @@
+n=input('Digite um nome de usuario:')
+s=input('Digite uma senha:')
+while n==s:
+    print('Senha invalida')
+    s=input('Digite uma senha diferente do nome de usuario:')
+print('Registrado')
